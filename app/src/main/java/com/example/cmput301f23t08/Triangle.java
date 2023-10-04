a852ee72f1149b109045f18472dfa1b0c8ab1107
Matthew Neufeld <matthewneufeld@Matthews-MacBook-Pro.local>
2023-10-03 18:06:52 -0600

    Added triangle

--- app/src/main/java/com/example/cmput301f23t08/Triangle.java
+++ app/src/main/java/com/example/cmput301f23t08/Triangle.java
@@ -1,2 +1,5 @@
-package com.example.cmput301f23t08;public class Triangle {
+package com.example.cmput301f23t08;
+
+public class Triangle extends Shape {
+    private double angle1, angle2, angle3;
 }
